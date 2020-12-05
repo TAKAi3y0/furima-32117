2dd2ef9510df8a9ae640720695467d7c7d057317
TAKAi3y0 <73812712+TAKAi3y0@users.noreply.github.com>
2020-12-05 20:58:56 +0900

    1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @products = Product.all.includes(:user).order(id: :DESC)
@@ -7,10 +8,6 @@ class ItemsController < ApplicationController
 
   def new
     @product = Product.new
-  end
-
-  def show
-    @product = Product.find(params[:id])
   end
 
   def create
@@ -22,9 +19,32 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to item_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :explanation, :category_id, :area_id, :charge_id, :send_date_id, :status_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @product = Product.find(params[:id])
+    redirect_to action: :index unless @product.user == current_user # || @product.buy.nil?
   end
 end
